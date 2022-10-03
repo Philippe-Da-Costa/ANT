f8df4c5786749885bcbd1fbf49afe51035c86c35
Da costa Philippe <Dacosta-philippe@laposte.net>
2022-10-03 17:04:24 +0200

    mon tp

---
+++ TP_ANT fiipe/tp3/creerArbo.bat
@@ -0,0 +1,15 @@
+echo off
+	cls
+	md D:\TP_ANT\tp3\r1
+	d:
+	cd D:\TP_ANT\tp3\r1
+	copy nul toto.txt
+	copy nul ab12.txt
+	copy nul tutu.txt
+	copy nul titi.txt
+	copy nul trivial.txt
+	copy nul t632.txt
+	copy nul tbc2.txt
+	copy nul tnt.txt
+	copy nul t.txt
+pause
